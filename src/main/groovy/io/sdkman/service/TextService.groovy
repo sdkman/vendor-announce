82694ac7eb29b7ac9698d5e6571cfbd3c902d19a
Marco Vermeulen <vermeulen.mp@gmail.com>
2020-09-20 20:56:07 +0100

    Remove dead code in text service.

--- src/main/groovy/io/sdkman/service/TextService.groovy
+++ src/main/groovy/io/sdkman/service/TextService.groovy
@@ -15,40 +15,10 @@
  */
 package io.sdkman.service
 
-import io.sdkman.domain.Broadcast
 import org.springframework.stereotype.Service
-
-import static java.text.DateFormat.SHORT
-import static java.text.DateFormat.getDateInstance
 
 @Service
 class TextService {
-
-    static final HEADER = '==== BROADCAST ================================================================='
-    static final FOOTER = '================================================================================'
-    static final NO_BROADCASTS = "No broadcasts available at present.\n"
-
-    String prepare(Broadcast broadcast) {
-        prepare([broadcast])
-    }
-
-    String prepare(ArrayList<Broadcast> broadcasts) {
-        def output = "$HEADER\n"
-        if(broadcasts)
-            output += buildMessage(broadcasts)
-        else
-            output += NO_BROADCASTS
-        "${output}$FOOTER"
-    }
-
-    private static buildMessage(broadcasts) {
-        def output = ""
-        broadcasts.each { broadcast ->
-            output += "* ${getDateInstance(SHORT, Locale.UK).format(broadcast.date)}: $broadcast.text\n"
-        }
-        output
-    }
-
     String composeStructuredMessage(String candidate, String version, String hashtag = candidate) {
         def prefixedHashtag = hashtag.startsWith("#") ? hashtag : "#$hashtag"
         "${candidate.toLowerCase().capitalize()} $version released on SDKMAN! $prefixedHashtag"
--- src/test/groovy/io/sdkman/service/TextServiceSpec.groovy
+++ src/test/groovy/io/sdkman/service/TextServiceSpec.groovy
@@ -15,7 +15,6 @@
  */
 package io.sdkman.service
 
-import io.sdkman.domain.Broadcast
 import spock.lang.Specification
 
 import static io.sdkman.service.TextService.*
@@ -24,74 +23,8 @@ class TextServiceSpec extends Specification {
 
     TextService service
 
-    Broadcast broadcast1 = new Broadcast(id: 1, text: "text1", date: new Date())
-    Broadcast broadcast2 = new Broadcast(id: 2, text: "text2", date: new Date())
-
     void setup() {
         service = new TextService()
-    }
-
-    void "should add header line when preparing broadcast text"() {
-        given:
-        def broadcasts = [broadcast1, broadcast2]
-
-        when:
-        def text = service.prepare(broadcasts)
-        def firstLine = text.readLines().first()
-
-        then:
-        firstLine == '==== BROADCAST ================================================================='
-    }
-
-    void "should add footer line when preparing broadcast text"() {
-        given:
-        def broadcasts = [broadcast1, broadcast2]
-
-        when:
-        def text = service.prepare(broadcasts)
-        def lastLine = text.readLines().last()
-
-        then:
-        lastLine == '================================================================================'
-    }
-
-    void "should place each string on a new line"() {
-        given:
-        def broadcasts = [broadcast1, broadcast2]
-
-        when:
-        def text = service.prepare(broadcasts)
-        def readLines = text.readLines()
-
-        then:
-        readLines.size() == 4
-        readLines[0] == HEADER
-        readLines[1].contains(broadcast1.text)
-        readLines[2].contains(broadcast2.text)
-        readLines[3] == FOOTER
-    }
-
-    void "should allow conversion of single broadcasts"() {
-        when:
-        def text = service.prepare(broadcast1)
-        def readLines = text.readLines()
-
-        then:
-        readLines.size() == 3
-        readLines[0] == HEADER
-        readLines[1].contains(broadcast1.text)
-        readLines[2] == FOOTER
-    }
-
-    void "should render a friendly message if no broadcasts are passed in"() {
-        when:
-        def text = service.prepare([])
-        def readLines = text.readLines()
-
-        then:
-        readLines[0] == HEADER
-        readLines[1] == "No broadcasts available at present."
-        readLines[2] == FOOTER
     }
 
     void "should compose a structured release message line"() {
